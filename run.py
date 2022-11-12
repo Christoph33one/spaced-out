4bcf6a8848cfd3f64dd143ded4c29056972dd9bc
Christoph33one <cjbrowning23@hotmail.com>
2022-11-12 17:46:40 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Step 1. add a start game meassage
+
+"""
+
+
+def game_intro():
+    """
+    Game intro and message
+    """
+    print("Hello")
+
+
