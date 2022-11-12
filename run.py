b6a4c90bebbe942a4cf7e3b4d427465dee0a66fc
Christoph33one <cjbrowning23@hotmail.com>
2022-11-12 17:56:36 +0000

    test commit

--- run.py
+++ run.py
@@ -11,3 +11,27 @@ def game_intro():
     print("Hello")
 
 
+def users_name():
+    """
+    For the user to enter there name.
+    """
+
+    while True:
+        name = input("Please enter your name:")
+        if len(name) < 3:
+            print("Add four or more letters!")
+            continue
+        return name
+
+
+def main():
+    """
+    To run all game functions
+    """
+    game_intro()
+
+    name = users_name()
+    print("Lets begin!")
+
+
+main()
